15c8a76ebaac2428a03ef8c0c1c4e4bad3e63d86
Giovanny Jardim <gigiojardim11@gmail.com>
2024-04-08 09:21:41 -0300

    A GIGANTESCAMENTE GRANDE PRA KCT MURALHA

---
+++ src/components/GrandeMuralha.jsx
@@ -0,0 +1,29 @@
+import amongus from "../assets/A-Grande-Muralha-matt-damon-cosmonerd-capa-post.jpg"
+
+
+export default function GrandeMuralha() {
+    return (
+        <header>
+
+            <h2>o jogo original</h2>
+            <img src={amongus} alt="a poha da grande muralha caralhoooooooooooo" />
+    
+            <main>
+                <p>
+                    <a href="https://chat.openai.com/c/da4fa677-7017-43b3-8efa-1b34c91818f2">dddddddddddddddddddddddddd</a>
+
+                    <br/>
+                
+                    "A Grande Muralha" é um épico de fantasia e ação que combina elementos históricos com uma dose generosa de criatividade. Dirigido por Zhang Yimou e lançado em 2016, o filme mergulha os espectadores na grandiosidade e mistério da Grande Muralha da China, apresentando-a não apenas como uma estrutura defensiva, mas como o último bastião da humanidade contra uma ameaça sobrenatural.
+
+                    No centro da trama está William (interpretado por Matt Damon), um mercenário europeu em busca de riquezas além das fronteiras conhecidas. Junto com seu parceiro Tovar (interpretado por Pedro Pascal), ele se vê envolvido em uma batalha épica quando encontra a Grande Muralha e descobre seu propósito secreto.
+
+                    Ao longo do filme, somos levados a um espetáculo visual de cores vibrantes, coreografias de luta impressionantes e um cenário que mistura história com folclore. A Grande Muralha, construída para impedir a invasão de inimigos do norte, revela-se uma linha de defesa contra criaturas monstruosas e implacáveis.
+
+                    Com uma narrativa que equilibra ação emocionante com mitologia intrigante, "A Grande Muralha" oferece uma experiência cinematográfica única, transportando os espectadores para uma era de maravilhas e perigos além da imaginação.
+                </p>
+                    
+            </main>
+        </header> 
+         )
+    }
